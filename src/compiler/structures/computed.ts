fa23177d0ac93993efd7cedbf2cf583a55326135
Pedro Costa <pcosta@infraspeak.com>
2023-11-30 14:56:30 +0000

    https://infraspeak.atlassian.net/browse/ENG-3361

--- src/compiler/structures/computed.ts
+++ src/compiler/structures/computed.ts
@@ -11,11 +11,21 @@ export function createComputed(computed: GetAccessorDeclaration): Computed {
 export function generateComputed(computed: Computed, writer: CodeBlockWriter): CodeBlockWriter {
   buildDocs(computed, writer)
 
-  return writer
-    .write(`${computed.name}()${computed.returnType ? `: ${computed.returnType} ` : ' '}`).inlineBlock(() => {
-      (computed.statements as string[]).forEach(s => {
-        buildStatementDeclaration(s, writer)
-      })
+  writer.write(`${computed.name}()`)
+
+  if (computed.returnType) {
+    writer.write(`: ${computed.returnType} `)
+  } else {
+    writer.write(': TODO /** TODO: Add the missing return type. Otherwise, the build may fail. */')
+  }
+  
+  writer.inlineBlock(() => {
+    (computed.statements as string[]).forEach(s => {
+      buildStatementDeclaration(s, writer)
     })
-    .write(',').newLine()
+  })
+
+  writer.write(',').newLine()
+
+  return writer
 }
